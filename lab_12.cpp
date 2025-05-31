18349265e3188731b1d696f1ea15360ff633e698
VeronicaBalaziuk <veronikabalazuk16@gmail.com>
2025-05-31 20:02:06 +0300

    Add files via upload

---
+++ lab_12.cpp
@@ -0,0 +1,14 @@
+﻿#include "text_processor.h"
+#include <iostream>
+
+int main() {
+    TextProcessor::setTextFromInput();
+
+    int indexToRemove = TextProcessor::getRandomWordIndex();
+    std::cout << "Випадково вибране слово для вилучення: " << indexToRemove + 1 << std::endl;
+
+    TextProcessor::removeWord(indexToRemove);
+    TextProcessor::printText();
+
+    return 0;
+}
